f41f41c7cebc1ead4d8a73aa405ce9da309d278b
Caroline <cfharboe.dev@gmail.com>
2023-11-22 16:27:39 -0300

    Mais exercícios

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
-let num1 = prompt  ("Digite um número:");
-
+/*let num1 = prompt  ("Digite um número:");
 let num2 = prompt ("Digite outro número:");
-
 let op = prompt ("Digite operação matemática:");
-
 let soma;
-
 if (op == "+"){
     soma = Number(num1) + Number(num2);
 }else if(op == "-"){
@@ -17,41 +13,56 @@ if (op == "+"){
 }else{
     alert ('Informe uma op válida');
 }
-
-alert ('O resultado da op é'+soma);
-
-// let num1; declarando a variável
-// let num2; porém da para declarar já utilizando ela.
-
-// let num1 = prompt("Digite um número:");
-
-// let num2 = prompt("Digite outro número:");
-
-// let op = prompt ("Digite a operação matemática")
-
-// let soma;
-
-// if(op == "+"){
-//     soma = Number(num1) + Number(num2);   
-// }else if(op == '-'){
-//     soma = Number(num1) - Number(num2); 
-// }else if(op == '*'){
-//     soma = Number(num1) * Number(num2);
-// }else if(op == '/'){
-//     soma = Number(num1) / Number(num2);
-// }else{
-//     alert('Informe uma operação valida!')
-// }
-
-// alert('O resultado da operação é' +soma)
+alert ('O resultado da op é'+soma);*/
 
 
-let n1 = prompt ("Informe a primeira nota");
+/*let num1; declarando a variável
+let num2; porém da para declarar já utilizando ela.
+let num1 = prompt("Digite um número:");
+let num2 = prompt("Digite outro número:");
+let op = prompt ("Digite a operação matemática")
+let soma;
+if(op == "+"){
+soma = Number(num1) + Number(num2);   
+}else if(op == '-'){
+soma = Number(num1) - Number(num2); 
+}else if(op == '*'){
+soma = Number(num1) * Number(num2);
+}else if(op == '/'){
+soma = Number(num1) / Number(num2);
+}else{
+alert ('Informe uma operação valida!')
+}
+alert('O resultado da operação é' +soma)*/
+
+
+/*let n1 = prompt ("Informe a primeira nota");
 let n2 = prompt ("Informe a segunda nota");
 let n3 = prompt ("Informe a terceira nota");
 let n4 = prompt ("Informe a quarta nota");
 let media;
-
 media = (Number(n1) + Number(n2) + Number(n3) + Number(n4))/4;
+alert('O resultado da média é:' +media);*/
 
-alert('O resultado da média é: ' +media);
+
+/*let km = prompt ("Quantidade de km percorrido");
+let dias = prompt ("Quantidade de dias alugado");
+let total;
+total = (Number(km)*0.20) + (Number(dias)*90);
+alert('Valor total a pagar' +total);*/
+
+let dkm = prompt ("Digite a distância percorrida");
+
+let rs = prompt ("Preço da passagem");
+
+let multi;
+
+if(dkm <= 200){
+    multi = Number (dkm)*0.50;
+}else{
+    multi = Number (dkm)*0.45;
+}
+    alert("Total a pagar:" +multi)
+
+
+
