4f7a31b1dc1a1d30627b51830cc91ae5fcd08daa
Caroline <cfharboe.dev@gmail.com>
2023-11-29 17:23:26 -0300

    Adição de function

--- script.js
+++ script.js
@@ -182,3 +182,28 @@ alert( "O novo salário é: R$" + salf );*/
 // else{
 //     alert ("Obesidade mórbida");
 // }
+
+function mostrarTabuada() {
+//cria referência aos elementos da página
+let inNumero = document.getElementById('inNumero');
+let outTabuada = document.getElementById('outTabuada');
+
+//converte conteúdo do campo inNumero
+let numero = Number(inNumero.value);
+
+//cria uma variável do tipo String, que irá concatenar a resposta
+let resposta = " ";
+
+//cria um laço de repetição
+for (let i = 1; i <=10; i++) {
+    //a variável resposta vai acumulando os novos conteúdos
+    resposta = resposta + numero + " X " + i + "=" + numero * i + "\n"
+}
+
+//o conteúdo da tag pre é alterado para exibir a tabuada do num
+outTabuada.textContent = resposta;
+}
+//cria referência ao botão e após associa function ao evento click
+let btMostrar = document.getElementById('btMostrar');
+btMostrar.addEventListener("click", mostrarTabuada);
+ 
